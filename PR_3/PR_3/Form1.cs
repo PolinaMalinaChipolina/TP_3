be646c22b839eddf1033c62a6c6abbc308162697
Nastia <132841899+nastuuuuushaaaa@users.noreply.github.com>
2024-05-30 16:13:43 +0300

    Добавила возможность вычисления скачков валюты и построения графика с таблицами

--- PR_3/PR_3/Form1.cs
+++ PR_3/PR_3/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PR_3
 {
-    public partial class Form1 : Form
+    public partial class Controller : Form
     {
-        public Form1()
+        public Controller()
         {
             InitializeComponent();
         }
