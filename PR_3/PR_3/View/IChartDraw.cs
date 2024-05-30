4ac8301ffd9df89a0df885ffe9e14e847695688c
Polina <133887650+PolinaMalinaChipolina@users.noreply.github.com>
2024-05-30 15:15:16 +0300

    Добавила расчет численности населения и построение графика с таблицей

---
+++ PR_3/PR_3/View/IChartDraw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace GUI
+{
+    public interface IChartDraw
+    {
+        DataTable dataTable { get; set; }
+
+        void ShowChart(string[] titles); // Выводит на экран график [series]
+
+        void ShowAdditionalInfo(string Maininfo, string additionalInfo,bool append = false); // Выводит на экран сообщение [info]
+
+        void ShowGrid();// Выводит на экран таблицу
+
+    }
+}
---
+++ PR_3/PR_3/View/ViewForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public class ViewForm
+    {
+        protected Chart chart1;
+        protected RichTextBox richTextBox1;
+        protected DataGridView gridView1;
+
+        public  ViewForm(ref Chart c1, ref RichTextBox r1, ref DataGridView gridView1)
+        {
+            chart1 = c1;
+            richTextBox1 = r1;
+            this.gridView1 = gridView1;
+        }
+
+    }
+}
